c94117ef012bb96e6a7204c4acf14e8805024bf8
Alain Perez <aperez@mondragon.edu>
2013-02-07 17:10:57 +0100

    Settings activity created

--- ReviB/src/com/revib/revib/MainActivity.java
+++ ReviB/src/com/revib/revib/MainActivity.java
@@ -2,7 +2,9 @@ package com.revib.revib;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,17 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_settings:
+			Intent intent = new Intent(this, SettingsActivity.class);
+		    startActivity(intent);
+			return true;
+		}
+		return false;
 	}
 
 }

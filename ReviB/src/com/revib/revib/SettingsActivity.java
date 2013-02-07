e7f36fe2a5699916d1311b8a08729598e0d25fa1
Alain Perez <aperez@mondragon.edu>
2013-02-07 18:32:39 +0100

    Spinners added to settings activity

--- ReviB/src/com/revib/revib/SettingsActivity.java
+++ ReviB/src/com/revib/revib/SettingsActivity.java
@@ -1,9 +1,12 @@
 package com.revib.revib;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.support.v4.app.NavUtils;
 
 public class SettingsActivity extends Activity {
@@ -12,8 +15,29 @@ public class SettingsActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+		
+		Spinner spinner = (Spinner) findViewById(R.id.settings_languages_spnr);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.language_list, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
+		
+		spinner = (Spinner) findViewById(R.id.settings_sounds_spnr);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		adapter = ArrayAdapter.createFromResource(this,
+		        R.array.sound_list, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
+		
 		// Show the Up button in the action bar.
-		getActionBar().setDisplayHomeAsUpEnabled(true);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
 	}
 
 	@Override
